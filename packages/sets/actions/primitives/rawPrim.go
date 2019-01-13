29774f36c5fda4ca1c2a12166376c3077f9869ee
Christian <samsepi0l@gmx.de>
2019-01-13 12:51:09 +0100

    added new prims to parse

--- packages/sets/actions/primitives/rawPrim.go
+++ packages/sets/actions/primitives/rawPrim.go
@@ -13,6 +13,12 @@ func (y RawPrimitive) Parse() (Primitive, error) {
         return nil,errors.New("primitive contains no data")
     }
     switch y[0] {
+    case "ln":
+        return y.parseLn()
+    case "cp":
+        return y.parseCp()
+    case "run":
+        return y.parseRun()
     default:
         return nil,errors.New(fmt.Sprintf(
             "%s %s", y[0], "isnt a proper primitive",
