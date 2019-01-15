95b3507336d0a1806086d2725cd58a24810453e9
Christian <samsepi0l@gmx.de>
2019-01-15 15:36:22 +0100

    added getaction

--- packages/packages.go
+++ packages/packages.go
@@ -3,6 +3,8 @@ package packages
 import (
     "github/thosebeans/wento/packages/actions"
     "encoding/json"
+    "errors"
+    "fmt"
 )
 
 type Package struct {
@@ -14,4 +16,14 @@ func parsePackage(jsonBytes []byte) (Package, error) {
     var p Package
     var e error = json.Unmarshal(jsonBytes, &p)
     return p,e
+}
+
+func (y Package) GetAction(name string) (actions.Action, error) {
+    if y.Actions == nil || len(y.Actions) == 0 {
+        return actions.Action{},errors.New("package doesnt contain any actions")
+    } else if a := y.Actions[name]; a.Desc == "" && len(a.Primitives) == 0 {
+        return a,errors.New(fmt.Sprintf("%s %s", "package doesnt contain an action:", name))
+    } else {
+        return a,nil
+    }
 }
