57b89c4fbb0da033b957f9f3aed093d1f9b15e0d
Christian <samsepi0l@gmx.de>
2019-01-15 16:10:42 +0100

    added openPackage

---
+++ packages/open.go
@@ -0,0 +1,18 @@
+package packages
+
+import (
+    "github/thosebeans/wento/globals"
+    "path"
+    "os"
+    "io/ioutil"
+)
+
+func OpenPackage(name string) (Package, error) {
+    var filePath string = path.Join(globals.GetRootDir(), name, "package.json")
+    if f,e := ioutil.ReadFile(filePath); e != nil {
+        return Package{},e
+    } else {
+        defer os.Chdir(path.Join(globals.GetRootDir(), name))
+        return parsePackage(f)
+    }
+}
