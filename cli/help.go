a534613e835b5b3f58a30fb2437815fbaf78bc10
Christian Kämpfe <samsepi0l@gmx.de>
2019-01-22 20:18:12 +0100

    added new commands to help

--- cli/help.go
+++ cli/help.go
@@ -4,6 +4,10 @@ const helpStr string = `
 wento help          - show this
 wento init   <NAME> - create a new git-repo in ~/.wento/<NAME>
 wento remove <NAME> - remove package <NAME>
+wento clone  <URL>  - clone a git-repo in ~/.wento
+wento commit <PKG> <MSG> - commits all changes for <PKG>, <MSG> may be omitted
+wento pull   <PKG>  - pull changes for <PKG>
+wento push   <PKG>  - push changes of <PKG> to its remote repository
 wento list          - list local packages
 wento emerge <PKG> <ACTION> - emerge <ACTION> from <PKG>
 `
