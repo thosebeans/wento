656447bd8af9ba0eff93dc0229d02f5f87b64e57
Christian Kämpfe <samsepi0l@gmx.de>
2019-01-22 14:57:43 +0100

    added emerge-cli

---
+++ cli/emergePackage.go
@@ -0,0 +1,25 @@
+package cli
+
+import (
+    "os"
+    "path"
+    "errors"
+    "github/thosebeans/wento/globals"
+    "github/thosebeans/wento/packages"
+)
+
+func cliEmerge() error {
+    if len(os.Args) < 4 {
+        return errors.New("no enough arguments for emerge")
+    } else if p,e := packages.OpenPackage(os.Args[2]); e != nil {
+        return e
+    } else if a,e := p.GetAction(os.Args[3]); e != nil {
+        return e
+    } else if e := os.Chdir(path.Join(globals.GetRootDir(), os.Args[2])); e != nil {
+        return e
+    } else if e := a.Test(); e != nil {
+        return e
+    } else {
+        return a.Emerge()
+    }
+}
--- cli/parse.go
+++ cli/parse.go
@@ -9,5 +9,12 @@ func ParseCli() error {
     if len(os.Args) < 2 {
         return errors.New("no arguments given")
     }
-    return nil
+    var e error
+    switch os.Args[1] {
+    case "emerge":
+        return cliEmerge()
+    default:
+        return errors.New("no proper arguments given")
+    }
+    return e
 }
