b96c438bef13c714c343a31bdc2c7d5396b66b0a
Christian Kämpfe <samsepi0l@gmx.de>
2019-01-23 14:25:50 +0100

    existcheck to push

--- cli/push.go
+++ cli/push.go
@@ -11,6 +11,8 @@ import (
 func cliPush() error {
     if len(os.Args) < 3 {
         return errors.New("not enough arguments for push")
+    } else if e := packageExists(os.Args[2]); e != nil {
+        return e
     } else if e := os.Chdir(path.Join(globals.GetRootDir(), os.Args[2])); e != nil {
         return e
     }
