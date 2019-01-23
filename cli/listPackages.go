2bf543f007296cb822ec880669c6679bc064fc18
Christian Kämpfe <samsepi0l@gmx.de>
2019-01-23 15:09:39 +0100

    changed list to use GetPackages

--- cli/listPackages.go
+++ cli/listPackages.go
@@ -1,18 +1,15 @@
 package cli
 
 import (
-    "os"
     "github.com/thosebeans/wento/globals"
 )
 
 func cliLs() error {
-    if d,e := os.Open(globals.GetRootDir()); e != nil {
-        return e
-    } else if c,e := d.Readdirnames(-1); e != nil {
+    if s,e := globals.GetPackages(); e != nil {
         return e
     } else {
         println("Packages:")
-        for _,i := range c {
+        for _,i := range s {
             println(i)
         }
         return nil
