0e1c18e6701b3605261fbf5d37dd2b1a30ff37f3
Christian <samsepi0l@gmx.de>
2019-01-29 14:31:50 +0100

    added messages for test- adn emerge-errors

--- cli/emergePackage.go
+++ cli/emergePackage.go
@@ -6,6 +6,7 @@ import (
     "errors"
     "github.com/thosebeans/wento/globals"
     "github.com/thosebeans/wento/packages"
+    "fmt"
 )
 
 func cliEmerge() error {
@@ -20,8 +21,10 @@ func cliEmerge() error {
     } else if e := os.Chdir(path.Join(globals.GetRootDir(), os.Args[2])); e != nil {
         return e
     } else if e := a.Test(); e != nil {
-        return e
+        return errors.New(fmt.Sprintf("Test-Errors:\n%s\n", e.Error()))
+    } else if e := a.Emerge(); e != nil {
+        return errors.New(fmt.Sprintf("Emerge-Errors:\n%s\n", e.Error()))
     } else {
-        return a.Emerge()
+        return nil
     }
 }
